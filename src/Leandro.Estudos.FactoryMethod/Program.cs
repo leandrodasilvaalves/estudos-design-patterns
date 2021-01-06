862787a72ed2c116563b351ef3c9a748249e59ef
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-01-06 19:53:36 -0300

    Incluir projeto FactoryMethod

---
+++ src/Leandro.Estudos.FactoryMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leandro.Estudos.Patterns.FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
