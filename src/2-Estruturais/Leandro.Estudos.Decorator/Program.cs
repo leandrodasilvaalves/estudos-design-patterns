b52e9f68f42555d84ed3c2fea6a1d5f805001e76
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-01-10 09:31:03 -0300

    Incluir projeto para estudo do parttern Decorator

---
+++ src/2-Estruturais/Leandro.Estudos.Decorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leandro.Estudos.Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
